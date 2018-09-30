615e4215b8b50225937be23e0d67b704e4d52ebd
Chris <c.lamothe25@gmail.com>
2018-09-30 16:06:01 -0400

    Initial commit

---
+++ chapt2/tests.py
@@ -0,0 +1,49 @@
+# room_temp_c = 20
+# cooking_temp_f = 350
+# oven_heating_rate_c = 20
+# oven_heating_time = (
+#   ((cooking_temp_f - 32) * 5 / 9) - room_temp_c) / \
+#   oven_heating_rate_c
+# print(oven_heating_time)
+
+# exercise 1
+# print(9 - 3)
+# print(8 * 2.5)
+# print(9 / 2)
+# print(9 / -2)
+# print(9 // -2)
+# print(9 % 2)
+# print(9.0 % 2)
+# print(9 % 2.0)
+# print(9 % -2)
+# print(-9 % 2)
+# print(9 % -2.0)
+# print(4 + 3 * 5)
+# print((4 + 3) * 5)
+
+# exercise 3
+# temp = 24
+# temp = temp * 1.8 + 32
+# print(temp)
+
+#exercise 5
+# x = 10.5
+# y = 4
+# x = x + y
+# print(x)
+# print(y)
+
+# exercise 6
+# x = 3
+# x += x - x
+# print(x)
+
+# exercise 7 
+# print(10 + noob)
+
+#exercise 8
+print(6 * -----------8)
+# print(8 = people)
+print(((((4 ** 3)))))
+print((-(-(-(-5)))))
+# print(4 += 7/2)
